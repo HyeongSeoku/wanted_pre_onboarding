2b0d1e41611e23e307f48d4c0d3cf4927d69c663
HyeongSeoku <gudtjr3437@naver.com>
2022-04-21 16:04:18 +0900

    feat : Set Theme Color

---
+++ src/util/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  light: {
+    backgroundColor: "rgba(255,255,255,0.9)",
+    componentBgColor: "rgba(128,128,128,0.2)",
+    textColor: "rgba(0,0,0,0.8)",
+    mainColor: "rgba(3,218,197,0.7)",
+    toggleColor: "rgba(254,254,254)",
+    switchBgColor: "rgba(0,0,0,0.8)",
+  },
+  dark: {
+    backgroundColor: "rgba(0,0,0,0.8)",
+    componentBgColor: "rgba(255,255,255,0.3)",
+    textColor: "rgba(255,255,255,0.9)",
+    mainColor: "rgba(3,218,197,0.7)",
+    toggleColor: "rgba(2,2,2,0.4)",
+    switchBgColor: "rgba(255,255,255,0.9)",
+  },
+};
