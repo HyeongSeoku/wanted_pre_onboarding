7e8226b4ab702ea8fd2a765ed3e6e75bcbb0d7ef
HyeongSeoku <gudtjr3437@naver.com>
2022-04-21 16:40:17 +0900

    feat : Add tab data

--- src/constants/componentsData.js
+++ src/constants/componentsData.js
@@ -1,1 +1,11 @@
+import potatoImg from "../imgs/potato.png";
+import sweetPotatoImg from "../imgs/sweetPotato.png";
+import curyImg from "../imgs/cury.png";
+
 export const toggleData = ["기본", "상세"];
+
+export const tabData = [
+  { title: "감자", contents: potatoImg },
+  { title: "고구마", contents: sweetPotatoImg },
+  { title: "카레라이스", contents: curyImg },
+];
