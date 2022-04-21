c2be59c4be214f046f9fcbc33cc72630219afd12
HyeongSeoku <gudtjr3437@naver.com>
2022-04-22 02:27:39 +0900

    feat : Set font size

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Toggle from "./components/Toggle";
+import Toggle from "./components/toggle/Toggle";
 import styled from "styled-components";
 import { device, PC_MODE } from "./constants/standard";
 import Header from "./components/Header";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./util/theme";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { themeMode } from "./util/atoms";
+import Tab from "./components/tab/Tab";
 
 function App() {
   const mode = useRecoilValue(themeMode);
+
   return (
     <ThemeProvider theme={theme[mode]}>
       <div className="App">
@@ -18,6 +20,9 @@ function App() {
           <Header />
           <ComponentArea>
             <Toggle />
+          </ComponentArea>
+          <ComponentArea>
+            <Tab />
           </ComponentArea>
         </Container>
       </div>
@@ -31,15 +36,17 @@ const Container = styled.div`
   width: 100%;
   background-color: ${(props) => props.theme.backgroundColor};
   box-sizing: border-box;
-  padding: 8px 10px;
+  padding: 10px 15px;
   transition: all 0.4s ease;
   color: ${(props) => props.theme.textColor};
-
+  font-size: 1.2rem;
   @media ${device.laptop} {
     max-width: 50%;
+    font-size: 1.1rem;
   }
   @media ${device.desktop} {
     max-width: 50%;
+    font-size: 1.1rem;
   }
 `;
 
@@ -53,4 +60,6 @@ const ComponentArea = styled.div`
   border-radius: 8px;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 20px rgba(0, 0, 0, 0.1) inset;
   background-color: ${(props) => props.theme.componentAreaBgColor};
+  margin-bottom: 15px;
+  padding: 10px;
 `;
