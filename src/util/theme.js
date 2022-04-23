b6cf2ba0687d4e9312f734d8ac1f078f561f1d55
HyeongSeoku <gudtjr3437@naver.com>
2022-04-23 14:09:44 +0900

    style : Add color code

--- src/util/theme.js
+++ src/util/theme.js
@@ -7,6 +7,8 @@ export const theme = {
     toggleColor: "rgba(254,254,254)",
     switchBgColor: "rgba(0,0,0,0.8)",
     componentAreaBgColor: "rgba(254,254,254,0.1)",
+    componentTextColor: "rgba(128,128,128,0.9)",
+    headerBgColor: "rgba(127,127,127,0.1)",
   },
   dark: {
     backgroundColor: "rgba(0,0,0,0.8)",
@@ -16,5 +18,7 @@ export const theme = {
     toggleColor: "rgba(2,2,2,0.4)",
     switchBgColor: "rgba(255,255,255,0.9)",
     componentAreaBgColor: "rgba(2,2,2,0.5)",
+    componentTextColor: "rgba(255,255,255,0.5)",
+    headerBgColor: "rgba(0,0,0,0.7)",
   },
 };
